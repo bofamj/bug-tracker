63274e14a4488f34195740117af9aa84c924f5ef
unknown <mustafamjaber@yahoo.com>
2023-03-22 10:16:05 +0200

    add the dashboard page

---
+++ src/pages/DashBoard.jsx
@@ -0,0 +1,28 @@
+import { useEffect } from "react";
+import { ToastContainer, toast } from "react-toastify";
+import { useDispatch, useSelector } from "react-redux";
+
+const DashBoard = () => {
+  const { users } = useSelector((store) => store.users);
+
+  useEffect(() => {
+    toast.success("you have successfully logged in", {
+      position: "top-center",
+      autoClose: 5000,
+      hideProgressBar: false,
+      closeOnClick: true,
+      pauseOnHover: true,
+      draggable: true,
+      progress: undefined,
+      theme: "dark",
+    });
+  }, [users]);
+  return (
+    <>
+      <div>DashBoard</div>
+      <ToastContainer />
+    </>
+  );
+};
+
+export default DashBoard;
